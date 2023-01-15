6b6fc74827adb83af10e244a2ecf158c83e50cc0
unknown <dgkhorombi@gmail.com>
2023-01-16 00:49:38 +0200

    update the server url

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv);
 
-  const response = await fetch('http://localHost:5000', {
+  const response = await fetch('http://https://codemate-z357.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
